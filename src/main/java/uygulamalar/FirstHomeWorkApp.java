77a4c66ed2cf1f652580b1ffa65e5cfe7bbb97c4
ibrahimtkk <ibrahim.altay97@gmail.com>
2021-12-11 23:19:44 +0300

    [feature] 3. -> comment counts of all products

--- src/main/java/uygulamalar/FirstHomeWorkApp.java
+++ src/main/java/uygulamalar/FirstHomeWorkApp.java
@@ -1,19 +1,43 @@
 package uygulamalar;
 
 import dto.ProductCommentDto;
+import entity.Product;
 import entityservice.ProductCommentEntityService;
+import entityservice.ProductEntityService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FirstHomeWorkApp {
 
     public static void main(String[] args) {
 
-        ProductCommentEntityService service = new ProductCommentEntityService();
-        List<ProductCommentDto> productCommentList = service.findByProductId(4L);
+        final List<ProductCommentDto> productCommentList = commensOfOneProduct(4L);
+        final List<ProductCommentDto> productCommentDtoList = commentCountOfAllProducts();
+    }
 
-        for (ProductCommentDto productCommentDto : productCommentList) {
-            System.out.println(productCommentDto);
-        }
+    public static List<ProductCommentDto> commensOfOneProduct(Long productId) {
+        final ProductCommentEntityService service = new ProductCommentEntityService();
+        return service.findByProductId(productId);
+    }
+
+    public static List<ProductCommentDto> commentCountOfAllProducts() {
+        final ProductEntityService productEntityService = new ProductEntityService();
+        final ProductCommentEntityService productCommentEntityService = new ProductCommentEntityService();
+        final List<Product> products = productEntityService.findAll();
+
+        return products
+                        .stream()
+                        .map(product -> {
+                            List<ProductCommentDto> commentsOfOneProduct = productCommentEntityService.findByProductId(product.getId());
+                            return ProductCommentDto.builder()
+                                    .productId(product.getId())
+                                    .productName(product.getName())
+                                    .price(product.getPrice())
+                                    .commentCount(commentsOfOneProduct.size() > 0 ? commentsOfOneProduct.size() : null)
+                                    .build();
+
+                        })
+                        .collect(Collectors.toList());
     }
 }
